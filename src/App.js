5b049e57d93cd033b831c8daf3dccbf5e1cfd45e
Ropepe <pericavrhovac@gmail.com>
2021-11-25 18:33:15 +0100

    Add mui to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
-  return <div className="App">Ropepe</div>;
+  return (
+    <div className="App">
+      <h1>Ropepe</h1>
+      <Button variant="contained">Дугмић</Button>
+    </div>
+  );
 }
 
 export default App;
