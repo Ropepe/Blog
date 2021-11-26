224ad3e7f2f3406a92294ae28e282ffccb5ad59a
Ropepe <pericavrhovac@gmail.com>
2021-11-26 14:16:52 +0100

    Sredjivanje izgleda stranice

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import styles from "./Hero.module.css";
+
 const Hero = () => {
-  return <div className={styles.main}>Hero</div>;
+  return (
+    <div className={styles.main}>
+      <h1>Hero</h1>
+    </div>
+  );
 };
 
 export default Hero;
